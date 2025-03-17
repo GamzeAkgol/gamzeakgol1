a0b750a9cf5ae326ebfa8009f67cbda9587929ea
GamzeAkgol <akgolgamze@outlook.com>
2025-03-17 17:11:26 +0300

    Update HW1-Question2.cpp

--- HW1-Question2.cpp
+++ HW1-Question2.cpp
@@ -4,11 +4,11 @@
 /* function main begins program execution */
 int main( void )
 {
-   int integer1 ; /* first number to be input by user  */
-   int integer2 ; /* second number to be input by user */
-   int sum;      /* variable in which sum will be stored */ 
+   int integer1 ; /* first number to be input by user  */ //missing semicolon error!
+   int integer2 ; /* second number to be input by user */ //missing semicolon error!
+   int sum;      /* variable in which sum will be stored */  // We replaced the variable x with the variable sum!
 
-   printf( "Enter first integer\n" ); /* prompt */
+   printf( "Enter first integer\n" ); /* prompt */  //The punctuation mark was missing (" ")
    scanf( "%d", &integer1 );        /* read an integer */
 
    printf( "Enter second integer\n" ); /* prompt */
